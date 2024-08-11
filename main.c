0e1bc365eaf18a1289b031e40c767c16a888a4dc
MRSS02 <mateusyasd@hotmail.com>
2024-08-11 12:02:23 -0300

    atualizado para o novo repositório

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex();
+extern char* yytext; 
+
+int main () {
+
+    scanf("%s", yytext); // lê a linha a ser analizada pelo analizador léxico
+
+    token_t token = NUMBER; // define o token como NUMBER como um placeholder
+    while (token != EOL) { // roda enquanto não houver fim de linha 
+
+        token = yylex(); // chama yylex enquanto não houver quebra de linha
+        if (token >= 2 && token <= 5) { // se a token for um caractere de operação
+            printf("código do token: %d\n", token); // printa o código do token 
+        } else if (token == 6) { // caso inválida
+            printf(" token inválida\n"); // printa a invalidez
+        }
+    } 
+    printf("\n"); // quebra a linha após o término
+    return 0; // retorna sem erros
+}
---
+++ token.h
@@ -0,0 +1,10 @@
+typedef enum {
+
+        EOL,       // newline (final de linha)   
+        NUMBER,      // 1
+        PLUS,        // 2
+        MINUS,       // 3
+        TIMES,       // 4
+        DIV,         // 5
+        ERROR,       // 6
+} token_t; 
